947122e477f202763c3d5584f791de5f6a4029a3
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-07-07 19:30:09 +0530

    navbar was fixed at the top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         setActiveButton(buttonName);
     };
     return (
-        <nav className="flex justify-center items-center bg-black p-12">
+        <nav className="sticky top-0 z-20 flex justify-center items-center bg-black p-12">
             <ul className="flex gap-8 text-md font-medium text-white">
                 <li>
                     <a href="#about" onClick={() => { handleButtonClick('about') }} className={`${activeButton == 'about' ? 'border-customBg border p-3 rounded-lg' : ''}`}>
